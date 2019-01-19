78b684bf3efddd0970354b786a132d5d47856763
curlicue <>
2019-01-19 12:47:42 +0000

    initial

---
+++ app/src/main/java/curlicue/fluidicpager/MainActivity.kt
@@ -0,0 +1,13 @@
+package curlicue.fluidicpager
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.id.activity_main)
+    }
+
+}
